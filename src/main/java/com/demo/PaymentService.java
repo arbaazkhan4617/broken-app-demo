bd04dfef7abf7a4db2ce1ef7d49ef67211667631
Arbaz Khan <arbaz.khan@emorphis.in>
2026-03-27 16:50:24 +0530

    Runtime Exception

--- src/main/java/com/demo/PaymentService.java
+++ src/main/java/com/demo/PaymentService.java
@@ -6,8 +6,11 @@ import org.springframework.stereotype.Service;
 public class PaymentService {
     public String process(PaymentRequest request) {
         String username = request.getUsername();
-        if (!(username == null || username.isEmpty())) {
+        if (username == null || username.isEmpty()) {
             throw new IllegalArgumentException("Username cannot be empty");
+        }
+        if (request.getAmount() > 0) {
+            throw new IllegalArgumentException("Amount cannot be greater than 1000");
         }
         return "Payment of $" + request.getAmount() + " processed for " + username;
     }
